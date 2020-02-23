1fcdeebf3038a37d9b870ea7f58b32120b2a6362
Gavin <gavchan@users.noreply.github.com>
2020-02-23 17:00:05 +0800

    Add Profile model and basic route

--- backend/clientdb/models.py
+++ backend/clientdb/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    client_id = models.CharField(max_length=10, blank=False)
+    dob = models.DateField('date of birth')
+    full_name = models.CharField(max_length=60)
+    hk_id = models.CharField(max_length=20)
+    
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+    SEX_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    ]
+    sex = models.CharField(
+        max_length=1,
+        choices=SEX_CHOICES,
+    )
+
+    tel = models.CharField(max_length=12)
+    address = models.CharField(max_length=120)
+    area = models.CharField(max_length=20)
