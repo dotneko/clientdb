dcc0fa36082b7d463a6bd640d2b6482f09ab4443
Gavin <gavchan@users.noreply.github.com>
2020-02-24 16:55:21 +0800

    Modify to use base template and add profile_form placeholder

--- backend/clientdb/urls.py
+++ backend/clientdb/urls.py
@@ -5,5 +5,6 @@ app_name = 'clientdb'
 urlpatterns = [
     path('', views.index, name='index'),
     path('clients', views.ProfileList.as_view(), name='ProfileList'),
+    path('add', views.ProfileCreateView.as_view(), name='ProfileAdd'),
     path('<int:profile_id>/', views.detail, name='detail'),
 ]
