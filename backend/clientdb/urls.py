f76c3e20098991ccea4061117c184044e7d86689
Gavin <gavchan@users.noreply.github.com>
2020-02-24 15:10:16 +0800

    Add client profile list view

--- backend/clientdb/urls.py
+++ backend/clientdb/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'clientdb'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('clients', views.ProfileList.as_view(), name='ProfileList'),
     path('<int:profile_id>/', views.detail, name='detail'),
 ]
--- backend/clientdb/views.py
+++ backend/clientdb/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.views.generic import ListView
 from .models import Profile
 
 # Create your views here.
 
 def index(request):
-    latest_client_list = Profile.objects.order_by('-id')[:5]
+    latest_client_list = Profile.objects.order_by('id')[:5]
     context = {
         'latest_client_list': latest_client_list,
     }
     return render(request, 'clientdb/index.html', context)
+
+class ProfileList(ListView):
+    template_name = 'clientdb/profile_list.html'
+    model = Profile
+    context_object_name = 'latest_client_list'
+
+    def get_queryset(self):
+        """ Return last five clients."""
+        return Profile.objects.order_by('id')[:5]
 
 def detail(request, profile_id):
     client = get_object_or_404(Profile, pk=profile_id)
