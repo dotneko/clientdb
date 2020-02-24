7b864ed08fba180eb6c1652dda483fda62cbff91
Gavin <gavchan@users.noreply.github.com>
2020-02-24 11:48:42 +0800

    Basic index and detail views

--- backend/clientdb/urls.py
+++ backend/clientdb/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'clientdb'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:profile_id>/', views.detail, name='detail'),
 ]
--- backend/clientdb/views.py
+++ backend/clientdb/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Profile
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world. Index here.")
+    latest_client_list = Profile.objects.order_by('-id')[:5]
+    context = {
+        'latest_client_list': latest_client_list,
+    }
+    return render(request, 'clientdb/index.html', context)
+
+def detail(request, profile_id):
+    client = get_object_or_404(Profile, pk=profile_id)
+    return render(request, 'clientdb/detail.html', {'client': client })
