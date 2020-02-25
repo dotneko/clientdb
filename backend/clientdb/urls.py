23cd2d617cc060d8b057cf8dd45e657b58b0359d
Gavin <gavchan@users.noreply.github.com>
2020-02-25 18:29:16 +0800

    Use empty url for ListView

--- backend/clientdb/urls.py
+++ backend/clientdb/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'clientdb'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('clients', views.ClientList.as_view(), name='ClientList'),
+    # path('', views.index, name='index'),
     path('add', views.ClientCreateView.as_view(), name='ClientAdd'),
     path('<int:pk>/', views.ClientDetailView.as_view(), name='ClientDetail'),
+    path('', views.ClientList.as_view(), name='ClientList'),
 ]
