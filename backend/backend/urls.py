5ccbecbb8972f59fa81384b4905e8efead73d1aa
Gavin <gavchan@users.noreply.github.com>
2020-02-25 18:28:18 +0800

    Re-set empty url to point to clientdb

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('clientdb/', include('clientdb.urls')),
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls'))
+    path('user/', include('user.urls')),
+    path('', include('clientdb.urls')),
 ]
