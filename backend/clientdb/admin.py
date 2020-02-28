c52baf6ae6e948789021db49bb9a27eafead6873
Gavin <gavchan@users.noreply.github.com>
2020-02-28 12:56:33 +0800

    Add django-import-export

--- backend/clientdb/admin.py
+++ backend/clientdb/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Client
 
-admin.site.register(Client)
+class ClientResource(resources.ModelResource):
+
+    class Meta:
+        model = Client
+        
+class ClientAdmin(ImportExportModelAdmin):
+    resource_class = ClientResource
+
+admin.site.register(Client, ClientAdmin)
